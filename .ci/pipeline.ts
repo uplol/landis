ba85fce0a7274c012264e9f8fc113474ca5ffa37
andrei <b1naryth1ef@gmail.com>
2021-06-27 06:00:06 +0000

    ci: update pipeline

--- .ci/pipeline.ts
+++ .ci/pipeline.ts
@@ -1,29 +1,25 @@
-import { Workspace, pushStep, spawnChildJob } from "runtime/core.ts";
-import * as Docker from "pkg/buildy/docker@latest/mod.ts";
+import { pushStep, spawnChildJob, Workspace } from "runtime/core.ts";
+import * as Docker from "pkg/buildy/docker@1/mod.ts";
 import { readSecrets } from "runtime/secrets.ts";
 
-const image = "rust:1.48-buster";
+const image = "rust:1.53-buster";
 
 export async function run(ws: Workspace) {
-    const tag = `landis/landis:latest`;
+  const tag = `landis/landis:latest`;
 
-    pushStep(`Build Landis Image`);
-    await Docker.buildImage({
-        tag: tag,
-    });
+  pushStep(`Build Landis Image`);
+  await Docker.buildImage({
+    tag: tag,
+  });
 
-    pushStep(`Push Landis Image`);
-    const [githubUsername, githubToken] = await readSecrets(
-        "GITHUB_USERNAME",
-        "GITHUB_TOKEN",
-    );
+  pushStep(`Push Landis Image`);
+  const [githubUsername, githubToken] = await readSecrets(
+    "GITHUB_USERNAME",
+    "GITHUB_TOKEN"
+  );
 
-    await Docker.pushImage(
-        `${tag}`,
-        "docker.pkg.github.com/uplol",
-        {
-            username: githubUsername,
-            password: githubToken,
-        },
-    );
+  await Docker.pushImage(`${tag}`, "docker.pkg.github.com/uplol", {
+    username: githubUsername,
+    password: githubToken,
+  });
 }
